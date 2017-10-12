1b669ba704f4d34968ef105a511d1a3c5135de3c
digitalfabric92 <dwawryko@gmail.com>
2017-10-12 19:20:42 +0000

    Added user login handler

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// ================================================================================
+// Initialization and Require
+// ================================================================================
+
 'use_strict;'
-// ================================================================================
-// Initialization
-// ================================================================================
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -14,69 +15,177 @@ app.use(cookieParser());
 // View engine
 app.set("view engine", "ejs")
 
-var urlDatabase = {
+// ================================================================================
+// Database and objects
+// ================================================================================
+
+// Database of short URL keys and the long URLS
+let urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
-    var NUM ='';
-    var CDS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    for (var i = 1; i <= 6; i++){
-        NUM += CDS.charAt(Math.floor(Math.random() * CDS.length));
-    }
-    return "DF" + NUM;
-}
+// Users object
+let users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "dave": {
+    id: "dave",
+    email: "dwawryko@gmail.com",
+    password: "123456"
+  }
+};
 
-app.get('/urls', (req, res)=>{
-  let templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
-  res.render("urls_index", templateVars);
+
+// ================================================================================
+// Get requests
+// ================================================================================
+
+// Hello page
+app.get("/", (req, res) => {
+  res.end("Hello!");
 });
 
+// Sending urlDatabase data within the urls key
+app.get('/urls', (req, res)=>{
+  let templateVars =  {
+    urls: urlDatabase,
+    users: users,
+    user_id: req.cookies['user_id']
+  };
+  res.render('urls_index', templateVars);
+});
+
+// Get request form urls_new to create/add shortURLs
 app.get('/urls/new', (req, res)=>{
   let templateVars = {
-    username: req.cookies["username"]
+    users: users,
+    user_id: req.cookies['user_id']
   };
   res.render('urls_new', templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+// Returns registration page
+app.get("/register", (req, res) => {
+  let templateVars = {
+    users: users,
+    user_id: req.cookies['user_id']
+  };
+  res.render('register', templateVars);
 });
 
-app.get("/", (req, res) => {
-    res.end("Hello!");
+// Create a Login Page
+app.get('/login', (req, res)=>{
+  res.render('login' /*templateVars*/);
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
+    let templateVars = {
+      urls: urlDatabase,
+      users: users,
+      user_id: req.cookies['user_id']
+    };
     res.render("urls_index", templateVars);
 });
 
 app.get('/urls/new', (req, res)=>{
-    res.render('urls_new');
+    let templateVars = {
+      users: users,
+      user_id: req.cookies['user_id']
+    };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-    let longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
+  let templateVars =  {
+    users: users,
+    user_id: req.cookies['user_id']
+  };
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(longURL, templateVars);
+  } else {
+    res.send('Not Found!');
+  }
 });
 
-app.get("/hello", (req, res) => {
-    res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/urls/:id', (req, res)=>{
+  let templateVars =  {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    users: users,
+    user_id: req.cookies['user_id']
+  };
+  res.render('urls_show', templateVars);
 });
 
-// POST route that shortens URL
-app.post("/urls", (req, res) => {
+// ================================================================================
+// Unused gets
+// ================================================================================
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// // app.get("/hello", (req, res) => {
+//     res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// ================================================================================
+// Post Requests
+// ================================================================================
+
+// Register new user object in the global users
+app.post('/register', (req, res) => {
+  if (req.body.email && req.body.password) {
+      var user_id = generateRandomString();
+      var email = req.body.email;
+      var password = req.body.password;
+      res.cookie('user_id', user_id);
+      users[user_id] = { id:user_id, email:email, password:password};
+      res.redirect('/urls');
+  } else {
+    res.status(400).send('Please provide email and password');
+  }
+});
+
+// Login post request
+app.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  for (var KEY in users) {
+    if (users[KEY].email === email && users[KEY].password === password) {
+      console.log('If');
+      res.cookie('user_id', users[KEY].id);
+      res.redirect('/urls');
+    }
+  }
+  res.cookie('user_id', req.body.username);
+  res.redirect('/urls');
+});
+
+// Logout post request
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+});
+
+// Post route that shortens URL
+app.post('/urls', (req, res) => {
     var shortURL = generateRandomString();
     urlDatabase[shortURL] = req.body.longURL;
     res.redirect(302, '/urls/'+shortURL);
 });
 
-// POST route that updates a URL resource
+// Post route that updates a URL resource
 app.post('/urls/:id', (req, res)=>{
   var updatedURL = req.body.updatedURL;
   var shortURL = req.params.id;
@@ -84,34 +193,36 @@ app.post('/urls/:id', (req, res)=>{
   res.redirect('/urls/'+shortURL);
 })
 
-app.get('/urls/:id', (req, res)=>{
-  let templateVars =  {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    //username: req.cookies["username"]
-  };
-  res.render('urls_show', templateVars);
-});
-
-//POST route that removes a URL resource:
+//Post route that removes a URL resource:
 app.post('/urls/:id/delete', (req, res)=>{
     var deletedURL = req.params.id;
     delete urlDatabase[deletedURL];
     res.redirect('/urls');
 });
 
-// Login/Logout post requests
-app.post('/login', (req, res)=>{
-  res.cookie('username', req.body.username);
-  res.redirect('/urls')
-});
 
-app.post('/logout', (req, res)=>{
-  res.clearCookie('username')
-  res.redirect('/urls')
-});
 
-// SERVER listen
+
+// ================================================================================
+// Server listen
+// ================================================================================
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+// ================================================================================
+// Functions
+// ================================================================================
+
+// Generate alphanumeric string
+function generateRandomString() {
+  var NUM ='';
+  var CDS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (var i = 0; i < 6; i++){
+    NUM += CDS.charAt(Math.floor(Math.random() * (CDS.length + 1) ));
+  }
+  var POS = Math.floor(Math.random() * 6)+1;
+  var result = NUM.substr(0, POS) + "DF" + NUM.substr(POS);
+  return result;
+}
--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 function generateRandomString() {
     var NUM ='';
     var CDS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    for (var i = 1; i <= 6; i++){
+    for (var i = 0; i < 5; i++){
         NUM += CDS.charAt(Math.floor(Math.random() * CDS.length));
     }
-    return "DF" + NUM;
+    var POS = Math.floor(Math.random() * 6)+1;
+    console.log(POS);
+    var result = NUM.substr(0, POS) + "DF" + NUM.substr(POS);
+    return result;
 }
 console.log(generateRandomString());
